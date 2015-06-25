ce5f502b1974817c722c7b6929d28526734f01ab
Casey Lee <casele@vsp.com>
2015-06-24 23:31:27 -0700

    eureka

--- src/main/java/com/vspglobal/Application.java
+++ src/main/java/com/vspglobal/Application.java
@@ -3,9 +3,11 @@ package com.vspglobal;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @ComponentScan
 @EnableAutoConfiguration
+@EnableEurekaClient
 public class Application {
 
     public static void main(String[] args) {
